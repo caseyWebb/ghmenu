ee4f5ef992ec2cb72f206e3f281d10d0a59fc2ea
Casey Webb <notcaseywebb@gmail.com>
2018-12-14 15:31:04 -0600

    perf: Cache repository list

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { exec } from 'child_process'
+import * as fs from 'fs'
+import * as os from 'os'
+import * as path from 'path'
 import { promisify as pify } from 'util'
 import fetch from 'node-fetch'
 
@@ -22,10 +25,16 @@ async function getToken(): Promise<string> {
 }
 
 async function dmenu(options: string[]): Promise<string> {
-  const { stdout } = await pify(exec)(
-    `echo "${uniq(options).join('\n').replace(/"/g, '\\"')}" | dmenu -i`
-  )
-  return stdout.trim()
+  const uniqOpts = Array.from(new Set(options))
+  try {
+      const { stdout } = await pify(exec)(
+      `echo "${uniqOpts.join('\n').replace(/"/g, '\\"')}" | dmenu -i`
+    )
+    return stdout.trim()
+  } catch (e) {
+    process.exit()
+    return ''
+  }
 }
 
 async function queryGithub(token: string, query: string): Promise<any> {
@@ -41,29 +50,38 @@ async function queryGithub(token: string, query: string): Promise<any> {
   return data
 }
 
-async function fetchRepositories(token: string): Promise<Repo[]> {
-  const repoArguments = 'first: 100'
-  const repoSchema = `
-    nodes {
-      name: nameWithOwner
-      url
-    }
-  `
-  const data = await queryGithub(token, `
-    query {
-      viewer {
-        repositories(${repoArguments}) { ${repoSchema} }
-        repositoriesContributedTo(${repoArguments}) { ${repoSchema} }
+async function fetchRepositories(forceRefresh?: boolean): Promise<Repo[]> {
+  const cache = path.join(os.tmpdir(), 'ghmenu_repos')
+  if (!forceRefresh && fs.existsSync(cache)) {
+    const buf = await pify(fs.readFile)(cache)
+    return JSON.parse(buf.toString())
+  } else {
+    const token = await getToken()
+    const repoArguments = 'first: 100'
+    const repoSchema = `
+      nodes {
+        name: nameWithOwner
+        url
       }
-    }
-  `)
-  return [
-    ...data.viewer.repositories.nodes,
-    ...data.viewer.repositoriesContributedTo.nodes
-  ]
+    `
+    const data = await queryGithub(token, `
+      query {
+        viewer {
+          repositories(${repoArguments}) { ${repoSchema} }
+          repositoriesContributedTo(${repoArguments}) { ${repoSchema} }
+        }
+      }
+    `)
+    const repos = [
+      ...data.viewer.repositories.nodes,
+      ...data.viewer.repositoriesContributedTo.nodes
+    ]
+    await pify(fs.writeFile)(cache, JSON.stringify(repos))
+    return repos
+  }
 }
 
-async function fetchRepositoryData(token: string, repo: string): Promise<{
+async function fetchRepositoryData(repo: string): Promise<{
   issues: IssueOrPullRequest[]
   pulls: IssueOrPullRequest[]
 }> {
@@ -73,6 +91,7 @@ async function fetchRepositoryData(token: string, repo: string): Promise<{
     number
     url
   `
+  const token = await getToken()
   const data = await queryGithub(token, `
     query {
       viewer {
@@ -110,12 +129,8 @@ function formatGraphQLIssues(data: any): IssueOrPullRequest[] {
   return data.nodes.map((pr: any) => ({
     number: pr.number,
     url: pr.url,
-    displayText: `#${pr.number} - (${pr.title})`
+    displayText: `#${pr.number} - ${pr.title}`
   }))
-}
-
-function uniq(arr: string[]) {
-  return Array.from(new Set(arr))
 }
 
 async function open(url: string) {
@@ -127,8 +142,18 @@ async function promptResource(): Promise<string> {
 }
 
 async function promptRepos(repos: Repo[]): Promise<undefined | Repo> {
-  const stdout = await dmenu(repos.map((repo) => repo.name))
-  return repos.find((repo) => repo.name === stdout)
+  const refreshOpt = 'Refresh Repository List'
+  const options = [
+    ...repos.map((repo) => repo.name),
+    refreshOpt
+  ]
+  const stdout = await dmenu(options)
+  if (stdout === refreshOpt) {
+    repos = await fetchRepositories(true)
+    return promptRepos(repos)
+  } else {
+    return repos.find((repo) => repo.name === stdout)
+  }
 }
 
 async function promptIssuesOrPullRequests(issues: IssueOrPullRequest[]): Promise<undefined | IssueOrPullRequest> {
@@ -140,8 +165,7 @@ async function promptIssuesOrPullRequests(issues: IssueOrPullRequest[]): Promise
 
 async function main(): Promise<void> {
   try {
-    const token = await getToken()
-    const repos = await fetchRepositories(token)
+    const repos = await fetchRepositories()
     const selectedRepo = await promptRepos(repos)
     const selectedResource = await promptResource()
 
@@ -150,7 +174,7 @@ async function main(): Promise<void> {
     if (selectedResource === 'Open on GitHub') {
       open(selectedRepo.url)
     } else {
-      const data = await fetchRepositoryData(token, selectedRepo.name)
+      const data = await fetchRepositoryData(selectedRepo.name)
       let selectedIssueOrPull: undefined | { url: string }
       switch (selectedResource) {
         case 'Issues':
